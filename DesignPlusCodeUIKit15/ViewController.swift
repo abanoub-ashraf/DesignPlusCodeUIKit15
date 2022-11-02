6a372297258c1654a82a41576cb1db0ffd7283c5
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2022-11-02 20:23:49 +0200

    card view design

--- DesignPlusCodeUIKit15/ViewController.swift
+++ DesignPlusCodeUIKit15/ViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - UI
+
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    
+    // MARK: - LifeCycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        cardView.layer.cornerRadius = 30
+        cardView.layer.cornerCurve = .continuous
+        
+        cardView.layer.shadowColor = UIColor(named: "Shadow")!.cgColor
+        cardView.layer.shadowOpacity = 0.5
+        cardView.layer.shadowOffset = CGSize(width: 0, height: 10)
+        cardView.layer.shadowRadius = 20
+        
+        blurView.layer.cornerRadius = 30
+        blurView.layer.cornerCurve = .continuous
+        blurView.layer.masksToBounds = true
     }
 
 
