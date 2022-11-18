67652c4c9e83b4e4a71ea760149e6c35f5ddffb0
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2022-11-18 23:25:59 +0200

    integrate push notifications using firebase

--- DesignPlusCodeUIKit15/AppDelegate.swift
+++ DesignPlusCodeUIKit15/AppDelegate.swift
@@ -7,12 +7,41 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+
+        ///
+        /// register for notifications
+        ///
+        UNUserNotificationCenter.current().delegate = self
+        
+        ///
+        /// request permission for allow notifications
+        ///
+        UNUserNotificationCenter.current().requestAuthorization(options: [.badge, .alert, .sound]) { granted, error in
+            guard error == nil else {
+                print("Error: \(error!.localizedDescription)")
+                return
+            }
+            
+            print("Alow Notifications Permission granted")
+        }
+        
+        ///
+        /// to be able to receive and interact with the upcoming notifications from firebase
+        ///
+        Messaging.messaging().delegate = self
+        
+        ///
+        /// registers to receive remote notifications through Apple Push Notification service
+        ///
+        application.registerForRemoteNotifications()
+        
         return true
     }
 
@@ -26,4 +55,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
     }
 
+}
+
+///
+/// for interacting with the notif
+///
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    ///
+    /// allows us to do any work right when the notification is about to be presented
+    ///
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.sound, .badge, .banner])
+    }
+    
+    ///
+    /// allows us to do any work after the notification is received
+    ///
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        completionHandler()
+    }
+    
+    ///
+    /// - this tells that the app did register for apple push notifications service
+    /// - this tell the messaging which device to send notification to
+    ///
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+}
+
+///
+/// A protocol to handle token update or data message delivery from FCM
+///
+extension AppDelegate: MessagingDelegate {
+    
 }
